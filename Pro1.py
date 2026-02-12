161f7f1c3c93914447d2bd3cb75338a152189727
dhruv26-tech <dhruvhackathon287@gmail.com>
2026-02-12 14:54:24 +0530

    Add files via upload

---
+++ Pro1.py
@@ -0,0 +1,19 @@
+# Understanding variables, input (), print (), type() functions...
+
+
+name = input ("Enter Name: ")
+print (name)
+print (type (name))
+
+i = int (input ("Enter any integer: "))
+print (i)
+
+print (type (i))
+
+f = float (input ("Enter any floating number: "))
+print (f)
+print (type (f))
+
+b = bool (input ("Is 2 + 2 = 4 ? State True or False: "))
+print (b)
+print (type (b))
