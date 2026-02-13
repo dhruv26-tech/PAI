39ca49f120fac8871fd6490f60ce873089a41721
Dhruv Mahajan <dhruvhackathon287@gmail.com>
2026-02-13 23:56:27 +0530

    Add files via upload

---
+++ Pro2.py
@@ -0,0 +1,8 @@
+
+a = int (input ("Enter number a: "))
+b = int (input ("Enter number b: "))
+print ("Sum of a & b: ",(a+b))
+
+c = int (input ("Enter number c: "))
+d = int (input ("Enter number d: "))
+print ("Subtraction of d from c: ",(c-d))
